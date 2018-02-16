2c96c438f92dc9910b61f3c0a6e083dd080e324b
NyuTartaros <niuyunfei123@live.cn>
2018-02-16 10:09:36 +0800

    update comments

--- src/com/ys/pmyhost/serviceInterface/BurnServiceInterface.java
+++ src/com/ys/pmyhost/serviceInterface/BurnServiceInterface.java
@@ -11,16 +11,40 @@ import com.ys.pmyhost.entity.BurnRequest;
 import com.ys.pmyhost.entity.BurnResponse;
 import com.ys.pmyhost.entity.PrintLabelList;
 
+/**
+ * Interface of burn service as a WebService.
+ * @author NyuTartaros
+ *
+ */
 @WebService  
 @SOAPBinding(style = SOAPBinding.Style.RPC)
 public interface BurnServiceInterface {
 	
+	/**
+	 * Start the burn service.
+	 * @return true if success
+	 */
 	@WebMethod
 	public boolean startBurnService();
 	
+	/**
+	 * Stop the burn service.
+	 * @return true if seccess
+	 */
 	@WebMethod
 	public boolean stopBurnService();
 
+	/**
+	 * Set up a new burn job.
+	 * @param burnRequest - A BurnRequest instance contains job info
+	 * @param burnDataList - A BurnDataList instance contains all data of this 
+	 * burn job
+	 * @param dataSHA1 - SHA1 of all burn data, String type
+	 * @param printLabelList - A PrintLabelList instance contains all print label
+	 * @param labelSHA1 - SHA1 of all print label, String type
+	 * @return A BurnResponse instance contains job info if success, null if
+	 * failed
+	 */
 	@WebMethod
 	public BurnResponse setNewJob(
 			@WebParam(name="burnRequese") BurnRequest burnRequest
@@ -31,6 +55,11 @@ public interface BurnServiceInterface {
 			@WebParam(name="printLabelList") PrintLabelList printLabelList
 			, @WebParam(name="labelSHA1") String labelSHA1);
 	
+	/**
+	 * Get status of a given job.
+	 * @param burnRequest A BurnRequest instance contains job info
+	 * @return A BurnResponse instance contains job info
+	 */
 	@WebMethod
 	public BurnResponse getJobStatus(
 			@WebParam(name="burnRequese") BurnRequest burnRequest);
