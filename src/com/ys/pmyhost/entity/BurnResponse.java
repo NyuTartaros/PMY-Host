0fd560b603c97fc1a5edb5b720c057c2977d7955
NyuTartaros <niuyunfei123@live.cn>
2018-03-09 02:25:49 +0800

    update BurnResponse object

--- src/com/ys/pmyhost/entity/BurnResponse.java
+++ src/com/ys/pmyhost/entity/BurnResponse.java
@@ -2,4 +2,50 @@ package com.ys.pmyhost.entity;
 
 public class BurnResponse {
 
+	private String jobId;
+	private String volumeName;
+	private int copyNum;
+	private int burnSpeed;
+	private int statu;
+	
+	public BurnResponse(String jobId, String volumeName, int copyNum, int burnSpeed, int statu) {
+		super();
+		this.jobId = jobId;
+		this.volumeName = volumeName;
+		this.copyNum = copyNum;
+		this.burnSpeed = burnSpeed;
+		this.statu = statu;
+	}
+
+	public String getJobId() {
+		return jobId;
+	}
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+	public String getVolumeName() {
+		return volumeName;
+	}
+	public void setVolumeName(String volumeName) {
+		this.volumeName = volumeName;
+	}
+	public int getCopyNum() {
+		return copyNum;
+	}
+	public void setCopyNum(int copyNum) {
+		this.copyNum = copyNum;
+	}
+	public int getBurnSpeed() {
+		return burnSpeed;
+	}
+	public void setBurnSpeed(int burnSpeed) {
+		this.burnSpeed = burnSpeed;
+	}
+	public int getStatu() {
+		return statu;
+	}
+	public void setStatu(int statu) {
+		this.statu = statu;
+	}
+	
 }
